6d15e01159aa12918092ef9f4521ee6c53f453f1
Scott Elfstrom <scott.elfstrom@projekt202.com>
2018-09-05 21:09:06 -0500

    Updated to add Bambit

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 const logger = require('winston');
-const auth = require('./auth.json');
+const auth = require('./test-auth.json');
+
+const KEYWORD = auth.keyword || 'dorp';
 
 // Logging
 logger.remove(logger.transports.Console);
@@ -15,6 +17,7 @@ const bot = new Discord.Client({
 });
 
 let hasDorpped = false;
+let hasBambitted = false;
 
 // Setup
 bot.on('ready', function(event) {
@@ -26,7 +29,7 @@ bot.on('ready', function(event) {
 // specified message is read in a text channel that the bot is in.
 
 bot.on('message', message => {
-  if (message.content.toLowerCase().includes('dorp')) {
+  if (message.content.toLowerCase().includes(KEYWORD)) {
     logger.warn(hasDorpped);
     logger.info(message.author.username);
     // logger.info(channelID);
@@ -40,7 +43,7 @@ bot.on('message', message => {
         message.channel.send("Lame? Don't care. You're lame, " + message.author.username + ".", { files: ["http://scottelfstrom.com/stuff/dorp-bear.png"] });
       } else if (message.content.toLowerCase().includes('bear needs killing')) {
         message.channel.send("I still love you, " + message.author.username + ".", { files: ["http://scottelfstrom.com/stuff/dorp-bear.png"] });
-      } else if (message.content.toLowerCase().includes('dorp')) {
+      } else if (message.content.toLowerCase().includes(KEYWORD)) {
         message.channel.send("Don't care.", { files: ["http://scottelfstrom.com/stuff/dorp-bear.png"] });
       }
 
@@ -53,6 +56,22 @@ bot.on('message', message => {
     }
   }
 
-  });
+  if (message.content.toLowerCase().includes('gambit')) {
+    logger.warn(hasBambitted);
+    logger.info(message.author.username);
+    // logger.info(channelID);
+    logger.info(message.content);
+    // logger.info(message.mentions.users);
+
+    message.channel.send('Bambit!', { files: ["http://scottelfstrom.com/stuff/bambit.jpg"] });
+
+      hasDorpped = true;
+
+      setTimeout(() => {
+        logger.info('!hasBambitted');
+        hasBambitted = false;
+      }, 10000);
+    }
+});
 
 bot.login(auth.token);
